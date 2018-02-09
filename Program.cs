dcdeb7fbfee30dd83da545dd21b9a4ea61a7c4d3
zloben69 <zloben69@mail.ru>
2018-02-09 21:29:42 +0500

    g

--- Program.cs
+++ Program.cs
@@ -10,26 +10,20 @@ namespace DBGLOGReaderConsole
     public static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      List<byte[]> temp = Reader("/Users/zloben69/Projects/DBGLOGReaderConsole/DBGLOGReaderConsole/2017 08 20 0000 (Float).DAT");
-      foreach (var item in temp)
-      {
-        string str = "";
-        for (var x = 0; x != item.Length; x++)
-        {
-          char[] ca = new char[38];
-        }
-        Console.WriteLine(str);
-      }
+      Reader("/Users/zloben69/Projects/DBGLOGReaderConsole/DBGLOGReaderConsole/2017 08 20 0000 (Float).DAT");
     }
 
-    static List<byte[]> Reader(string file)
+    static void Reader(string file)
     {
-      Dictionary<string, string> data = new Dictionary<string, string>();
-      List<byte[]> list = new List<byte[]>();
       System.IO.BinaryReader fs = new BinaryReader(File.Open(file, FileMode.Open));
       bool flag = false;
       while (true)
       {
+        string date;
+        string tagindex;
+        double value;
+        long index;
+
         byte blyat = fs.ReadByte();
         if (blyat == 0x0D)
         {
@@ -37,8 +31,17 @@ namespace DBGLOGReaderConsole
         }
         else if (blyat == 0x20 & flag == true)
         {
-          list.Add(fs.ReadBytes(38));
-          //Console.WriteLine(fs.ReadBytes(2).ToString());
+          date = System.Text.Encoding.Default.GetString(fs.ReadBytes(16));
+					fs.ReadBytes(3);
+          tagindex = System.Text.Encoding.Default.GetString(fs.ReadBytes(2));
+          value = BitConverter.ToDouble(fs.ReadBytes(8), 0);
+          fs.ReadBytes(2);
+          index = BitConverter.ToInt32(fs.ReadBytes(6), 0);
+          Console.WriteLine("{0}, {1}, {2}, {3}, {4}", 
+                            date,
+                            tagindex,
+                            value, 
+                            index);
           break;
         }
         else if (blyat == 0x1A)
@@ -46,7 +49,6 @@ namespace DBGLOGReaderConsole
           break;
         }
       }
-      return list;
     }
   }
 }
